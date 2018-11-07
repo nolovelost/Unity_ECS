b58e74bcbf8948a9135b059f3ffa59c496f12faf
Pratyush B Singh <prats.bdr@gmail.com>
2018-11-07 02:22:59 +0000

    Made some simple housekeeping changes to code

--- Assets/_SCRIPTS/JobSystem/JobManager.cs
+++ Assets/_SCRIPTS/JobSystem/JobManager.cs
@@ -7,7 +7,7 @@ using Unity.Jobs;
 public class JobManager : MonoBehaviour
 {
     TransformAccessArray transforms;
-    JobCarTransform carJob;
+    JobCarTransform carJobTransform;
     JobHandle carJobHandle;
 
     [SerializeField]
@@ -41,9 +41,12 @@ public class JobManager : MonoBehaviour
         carJobHandle.Complete();
 
         if (Input.GetKeyDown("space"))
-            AddCars(carIncremementAmount);
+        {
+            AddCars();
+        }
 
-        carJob = new JobCarTransform()
+
+        carJobTransform = new JobCarTransform()
         {
             upperBound = upperBound,
             lowerBound = lowerBound,
@@ -51,24 +54,24 @@ public class JobManager : MonoBehaviour
             movementSpeed = movementSpeed
         };
 
-        carJobHandle = carJob.Schedule(transforms);
+        carJobHandle = carJobTransform.Schedule(transforms);
 
         JobHandle.ScheduleBatchedJobs();
     }
 
-    void AddCars(int amount)
+    void AddCars()
     {
         carJobHandle.Complete();
 
         transforms.capacity = transforms.length + carIncremementAmount;
 
-        for (int i = 0; i < amount; i++)
+        for (int i = 0; i < carIncremementAmount; i++)
         {
             float xVal = Random.Range(leftBound, rightBound);
             float yVal = Random.Range(upperBound, lowerBound);
             Vector3 pos = new Vector3(xVal, yVal, depth);
             Quaternion rot = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-            var obj = Instantiate(carPrefab, pos, rot) as GameObject;
+            GameObject obj = Instantiate(carPrefab, pos, rot) as GameObject;
             transforms.Add(obj.transform);
         }
     }
